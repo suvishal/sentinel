fdd4bb5a66db8fdcad2dc0ff706daf1ed522da26
Suvishal Kumar Sesetti <suvishalkumarsesetti@gmail.com>
2026-07-20 23:27:47 +0530

    feat: update the database

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,15 @@
-DATABASE_URL = (
-    "postgresql://postgres:password@localhost:5432/sentinel"
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://vishalkumar@localhost:5432/sentinel"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
+
+Base = declarative_base()
