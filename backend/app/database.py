9998d2ab3073a11249ac9c940efcf48a3070da13
Suvishal Kumar Sesetti <suvishalkumarsesetti@gmail.com>
2026-08-22 22:22:54 +0530

    feat: dockerised the application

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://vishalkumar@localhost:5432/sentinel"
+#DATABASE_URL = "postgresql://vishalkumar@localhost:5432/sentinel"
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://vishalkumar:sentinel@localhost:5432/sentinel"
+)
 
 engine = create_engine(DATABASE_URL)
 
